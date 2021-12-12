08c2f9800f2d89b0b8ee7060d14612c5b4a60fb6
helman101 <helmanandresortegon@gmail.com>
2021-12-12 13:59:08 +0100

    Create Navbar component

---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navbar = () => (
+  <nav>
+    <div>
+      <img className="icon" src="./assets/img/beer.png" alt="beer cup icon"/>
+    </div>
+  </nav>
+);
+
+export default Navbar;
