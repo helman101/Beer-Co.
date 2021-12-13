40b0dcaadb536a31c52d41ba044c8765ce31e09d
helman101 <helmanandresortegon@gmail.com>
2021-12-13 15:01:13 +0100

    create BeerList component

---
+++ src/container/BeerList.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import BeerPreview from '../components/BeerPreview';
+import { Pagination } from '../components/Pagination';
+
+const BeerList = () => {
+  const beerList = useSelector((state) => state.beers.currentList)
+  return (
+    <div className="d-flex flex-column justify-content-center">
+      <div className="d-flex flex-wrap beer-list">
+        {beerList.map(beer => (
+          <BeerPreview key={beer.beer_id} beer={beer} />
+        ))}
+      </div>
+      <Pagination />
+    </div>
+  )
+}
+
+export default BeerList;
