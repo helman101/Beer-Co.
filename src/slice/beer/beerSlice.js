cedf53f0b25eec8d84e9f2778fdb889802578e0e
helman101 <helmanandresortegon@gmail.com>
2021-12-13 15:00:01 +0100

    add nextPage & prevPage action to beer reducer

--- src/slice/beer/beerSlice.js
+++ src/slice/beer/beerSlice.js
@@ -10,19 +10,35 @@ const initialState = {
   currentList: beers.slice(0, 12),
 }
 
+const updateState = (state) => {
+  state.lastIndex = state.currentPage * state.beersPerPage;
+  state.firstIndex = state.lastIndex - state.beersPerPage
+  state.currentList = state.fullList.slice(state.firstIndex, state.lastIndex)
+}
+
 export const beersSlice = createSlice({
   name: 'beers',
   initialState,
   reducers: {
     changePage: (state, action) => {
       state.currentPage = action.payload;
-      state.lastIndex = state.currentPage * state.beersPerPage;
-      state.firstIndex = state.lastIndex - state.beersPerPage
-      state.currentList = state.fullList.slice(state.firstIndex, state.lastIndex)
+      updateState(state);
     },
+    nextPage: (state) => {
+      if (state.lastIndex < state.fullList.length) {
+        state.currentPage += 1;
+        updateState(state);
+      }
+    },
+    previousPage: (state) => {
+      if (state.firstIndex > 0) {
+        state.currentPage -= 1;
+        updateState(state);
+      }
+    }
   },
 })
 
-export const { changePage } = beersSlice.actions
+export const { changePage, nextPage, previousPage } = beersSlice.actions
 
 export default beersSlice.reducer
