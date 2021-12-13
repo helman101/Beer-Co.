e3439cd8fa81ea33653dbd0d9555da445d91b847
helman101 <helmanandresortegon@gmail.com>
2021-12-13 19:48:01 +0100

    add saleFilter, priceFilter & typeFilter reducers

--- src/slice/beer/beerSlice.js
+++ src/slice/beer/beerSlice.js
@@ -8,6 +8,9 @@ const initialState = {
   lastIndex: 12,
   firstIndex: 0,
   currentList: beers.slice(0, 12),
+  onSaleFilter: false,
+  priceFilter: null,
+  typeFilter: null,
 }
 
 const updateState = (state) => {
@@ -35,10 +38,51 @@ export const beersSlice = createSlice({
         state.currentPage -= 1;
         updateState(state);
       }
+    },
+    saleFilter: (state) => {
+      if (state.onSaleFilter) {
+        state.onSaleFilter = false
+        state.fullList = [...beers]
+        state.currentPage = 1;
+        updateState(state)
+      } else {
+        state.onSaleFilter = true;
+        state.fullList = [...beers].filter((beer) => beer.on_sale)
+        state.currentPage = 1;
+        updateState(state)
+      }
+    },
+    priceFilter: (state, action) => {
+      if (action.payload === null) {
+        state.fullList = [...beers]
+        state.priceFilter = null
+      } else if (state.typeFilter) {
+        state.fullList = [...beers].filter((beer) => ((parseFloat(beer.price) <= action.payload.max && parseFloat(beer.price) >= action.payload.min) && beer.type === state.typeFilter))
+        state.priceFilter = action.payload
+      } else {
+        state.fullList = [...beers].filter((beer) => (parseFloat(beer.price) <= action.payload.max && parseFloat(beer.price) >= action.payload.min))
+        state.priceFilter = action.payload
+      }
+      state.currentPage = 1
+      updateState(state)
+    },
+    typeFilter: (state, action) => {
+      if (action.payload === 'Senza filtro') {
+        state.fullList = [...beers];
+        state.typeFilter = null;
+      } else if (state.priceFilter) {
+        state.fullList = state.fullList.filter((beer) => beer.type === action.payload);
+        state.typeFilter = action.payload;
+      } else {
+        state.fullList = [...beers].filter((beer) => beer.type === action.payload);
+        state.typeFilter = action.payload;
+      }
+      state.currentPage = 1
+      updateState(state)
     }
   },
 })
 
-export const { changePage, nextPage, previousPage } = beersSlice.actions
+export const { changePage, nextPage, previousPage, saleFilter, priceFilter, typeFilter } = beersSlice.actions
 
 export default beersSlice.reducer
