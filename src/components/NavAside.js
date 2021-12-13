6a70e212d93052365b7c20ee62d8c718822bdc1c
helman101 <helmanandresortegon@gmail.com>
2021-12-13 19:46:06 +0100

    create NavAside component

--- src/components/NavAside.js
+++ src/components/NavAside.js
@@ -1,32 +1,96 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { saleFilter, priceFilter, typeFilter } from '../slice/beer/beerSlice'
 
-const NavAside = ({ types }) => {
-    return (
+const NavAside = () => {
+  const onSale = useSelector((state) => state.beers.onSaleFilter)
+  const dispatch = useDispatch()
+
+  const handlePriceClick = (e) => {
+    switch (e.target.id) {
+      case '1':
+        dispatch(priceFilter({max: 20, min: 0}))
+        break;
+      case '2':
+        dispatch(priceFilter({max: 50, min: 20}))
+        break;
+      case '3':
+        dispatch(priceFilter({max: 100, min: 50}))
+        break;
+      case '4':
+        dispatch(priceFilter({max: 200, min: 100}))
+        break;
+      case '5':
+        dispatch(priceFilter({max: 10000, min: 200}))
+        break;
+      default:
+        dispatch(priceFilter(null))
+        break;
+    }
+  }
+
+  const handleTypeClick = (e) => {
+    dispatch(typeFilter(e.target.innerText))
+  }
+
+  const types = [
+    {
+        "type": "Lager",
+        "id": 127
+    },
+    {
+        "type": "Malt",
+        "id": 133
+    },
+    {
+        "type": "Ale",
+        "id": 137
+    },
+    {
+        "type": "Flavoured Malt",
+        "id": 141
+    },
+    {
+        "type": "Non-Alcoholic Beer",
+        "id": 180
+    },
+    {
+        "type": "Stout",
+        "id": 581
+    },
+    {
+        "type": "Porter",
+        "id": 936
+    }
+  ]
+
+  return (
     <div>
       <details className="nav-aside-item">
-        <summary>DISPONIBILITA</summary>
-        <div>
-          {/* <label for="sale">Sale</label>
-          <input name="sale" type="checkbox">In Sconto</input> */}
+        <summary>Disponibilita</summary>
+        <div className="aside-list">
+          <input onChange={() => dispatch(saleFilter())} name="sale" type="checkbox" checked={onSale}/>
+          <label>In Sconto</label>
         </div>
       </details>
       <details className="nav-aside-item">
-        <summary>PREZZO</summary>
-        <div>
-          {/* <input type="radio">Senza filtro</input>
-          <input type="radio">A meno di 20 EUR</input>
-          <input type="radio">20 - 50 EUR</input>
-          <input type="radio">50 - 100 EUR</input>
-          <input type="radio">100 - 200 EUR</input> */}
+        <summary>Prezzo</summary>
+        <div className="aside-list" onClick={handlePriceClick}>
+          <div id="0">Senza filtro</div>
+          <div id="1">A meno di 20 EUR</div>
+          <div id="2">20 - 50 EUR</div>
+          <div id="3">50 - 100 EUR</div>
+          <div id="4">100 - 200 EUR</div>
+          <div id="5">200 EUR e piu</div>
         </div>
       </details>
       <details className="nav-aside-item">
-        <summary>TIPO</summary>
-        <div>
-          {/* <input type="radio">Senza filtro</input> */}
-          {/* {types.map((type) => (
-            <input type="radio">{type}</input>
-          ))} */}
+        <summary>Tipo</summary>
+        <div className="aside-list" onClick={handleTypeClick}>
+          <div>Senza filtro</div>
+          {types.length > 0 && types.map((type) => (
+            <div key={type.id}>{type.type}</div>
+          ))}
         </div>
       </details>
     </div>
