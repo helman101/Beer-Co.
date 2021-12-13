cc160c299759f898a706d17c54d9b49714db85db
helman101 <helmanandresortegon@gmail.com>
2021-12-13 16:02:49 +0100

    add new itemReducer to store

--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
 import beersReducer from '../slice/beer/beerSlice'
+import itemReduce from '../slice/item/itemSlice'
 
 export default configureStore({
   reducer: {
     beers: beersReducer,
+    item: itemReduce,
   },
 })
