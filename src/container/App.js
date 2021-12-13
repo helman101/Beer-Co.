f9ec76972adae0492b06c5ea91c0e22a7bcf605c
helman101 <helmanandresortegon@gmail.com>
2021-12-13 11:30:30 +0100

    add Routes and Navbar to App component

--- src/container/App.js
+++ src/container/App.js
@@ -1,8 +1,19 @@
-import './App.css';
+import Navbar from '../components/Navbar';
+import { Route, Routes } from 'react-router';
+import Home from './Home';
+import BeerPage from './BeerPage';
+import '../assets/styles/App.css';
 
 const App = () => {
   return (
-    <div className="App">
+    <div>
+      <Navbar />
+      <div>
+        <Routes>
+          <Route exact path="" element={<Home />} />
+          <Route path="/Beer" element={<BeerPage />} />
+        </Routes>
+      </div>
     </div>
   );
 }
