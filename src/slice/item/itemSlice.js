5fa12ec5192423f57e5445ff9536be44e2191fd1
helman101 <helmanandresortegon@gmail.com>
2021-12-13 16:02:22 +0100

    add itemSlice

---
+++ src/slice/item/itemSlice.js
@@ -0,0 +1,19 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = {
+  currentBeer: {},
+}
+
+export const itemSlice = createSlice({
+  name: 'item',
+  initialState,
+  reducers: {
+    changeItem: (state, action) => {
+      state.currentBeer = action.payload
+    },
+  },
+})
+
+export const { changeItem } = itemSlice.actions
+
+export default itemSlice.reducer
